ffb0c31d467a7af76166362125dfa30b8f58ea86
miyeon396 <miyeon396@gmail.com>
2022-03-16 11:44:32 +0900

    add sample template

---
+++ src/main/java/com/example/codete/Template.java
@@ -0,0 +1,19 @@
+package com.example.codete;
+
+import java.util.Scanner;
+
+public class Template {
+    public String solution(String str) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String args[]) {
+        Template T = new Template();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(T.solution(str));
+
+    }
+}
--- src/main/java/com/example/codete/section1string/test.java
+++
@@ -1,9 +0,0 @@
-package com.example.codete.section1string;
-
-public class test {
-    public static void main(String[] args) {
-
-        System.out.println("test project");
-
-    }
-}
