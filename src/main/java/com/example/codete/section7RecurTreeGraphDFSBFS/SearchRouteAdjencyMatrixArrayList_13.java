44dadc06f66c47fa0cc415d0e5aa101dd6d6c794
miyeon396 <miyeon396@gmail.com>
2022-06-26 13:22:39 +0900

    그래프 최단 거리 예시 BFS
    - 1번 정점에서 각 정점으로 이동하는 최소 간선수
    - 배열로 해보는 예제

--- src/main/java/com/example/codete/section7RecurTreeGraphDFSBFS/SearchRouteAdjencyMatrixArrayList_13.java
+++ src/main/java/com/example/codete/section7RecurTreeGraphDFSBFS/SearchRouteAdjencyMatrixArrayList_13.java
@@ -1,56 +1,56 @@
 package com.example.codete.section7RecurTreeGraphDFSBFS;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class SearchRouteAdjencyMatrixArrayList_13 {
 
-    // 경로 탐색(인접리스트) (앞문제와 문제는 똑같은데 이건 인접 리스트 앞에껀 인접 행렬)
-    // 정점이 수만개가 들어오면 인접 행렬은 굉장히 비효율 적이다 행렬의 행과 열을 for문 다 돌면서 체크해야하기 떄문 메모리를 너무 크게 잡아먹는다 배열만드는데.. -> 인접 리스트를 사용
+    // 그래프 최단거리 (BFS) -> 최단거리는 BFS다  (1번에서 한번만에 가는 정점 2번만에 가는 정점 다 일케 찾으면 댄당!)
+    // 다음 그래프에서 1번 정점에서 각 정점으로 가는 최소 이동 간선수를 출력하세요
+    // 1 <- 2 -> 5
+    // |\    ^ ^ ^
+    // V V    X  |
+    // 3 -> 4 -> 6
 
-    // 방향 그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성
-    // 1 2 3 4 5 / 1 2 5 / 1 3 4 2 5 / 1 3 4 5 / 1 4 2 5 / 1 4 5
-    // 총 6가지 (그래프에서 1번정점에서 5버 정점으로 가는 가지수 )
-    //   <-
-    // 1 -> 2 -> 5
-    // | X  ^   ^
-    // VV  V|  /
-    // 3 -> 4
+//    ex) 0 -   1
+//        1 - 3   4
+//        2-    5   6
+//        3          2 // -> 이렇게 레벨별로 할 수도 있찌만 이 예제에서는 배열을 사용해서 함
+    // 배열로 하는 예제
+    // dis(arr) 1 2 3 4 5 6 (dis[n]은 1번 정점에서 각 배열 정점까지 가는 최소 거리구해서 넣어줌 ) => dis[nv] = dis[v]+1
+    // dis 0 3 1 1 2 2 일케 됨. BFS는 절대 돌아서 안감.
 
-    // 인접 리스트를 5개 만들어 놓음
-    // 1번 리스트 -> 2추가 -> 3추가 -> 4추가
-    // 2번 리스트 -> 1추가 -> 3추가
-    // 3번 리스트 -> 2추가 -> 5추가
-    //... 이런식으로
-
-    // 입력 설명
-    // 첫번째 줄에는 정점의 수 N (1<=N<=20)와 간선의 수 M 가 주어짐.
-    // 그 다음 부터 M 줄에 걸쳐 연결 정보가 주어진다.
-    // 출력 설명
-    // 총 가지수를 출력
-
-    // ex) 5 9 / 1 2 / 1 3 / 1 4 / 2 1 / 2 3 / 2 5 / 3 4/ 4 2 / 4 5 -> 6
-    // 경로 : 한번 방문한 노드는 다시 방문하지 않는다.
+    // 입력 설정
+    // 첫째 줄에는 정점의 수 N(1<=N<=20)와 간선의 수 M가 주어진다. 그 다음부터 M 줄에 걸쳐 연결정보가 주어진다.
+    // ex ) 6 9 / 1 3 / 1 4 / 2 1 / 2 5 / 3 4 / 4 5 / 4 6 / 6 2 / 6 5
+    // -> 2:3 / 3:1 / 4:1 / 5:2 / 6 : 2
 
     static int n = 0;
     static int m = 0;
     static int answer = 0;
     static ArrayList<ArrayList<Integer>> graph;
     static int[] ch;
+    static int[] dis;
 
-    public void DFS(int v){
-        if(v==n) {
-            answer++;
-        } else {
-            for (int nextV : graph.get(v)) { //v번 ArrayList
-                //이제 방문했냐 안했냐만 체크
-                if(ch[nextV]==0) { //no 방문
-                    ch[nextV] = 1;
-                    DFS(nextV);
-                    ch[nextV] = 0;
+    public void BFS(int v){
+        Queue<Integer> queue = new LinkedList<>();
+        ch[v] = 1;
+        dis[v] = 0;
+        queue.offer(v);
+        while(!queue.isEmpty()) {
+            int cv = queue.poll();
+            for(int nv : graph.get(cv)) {
+                //방문햇냐 안햇냐만 보면 됨.
+                if(ch[nv] == 0) {
+                    ch[nv] = 1;
+                    queue.offer(nv);
+                    dis[nv] = dis[cv]+1;
                 }
             }
         }
+
     }
 
     public static void main(String args[]) {
@@ -63,13 +63,15 @@ public class SearchRouteAdjencyMatrixArrayList_13 {
             graph.add(new ArrayList<Integer>());
         }
         ch = new int[n+1];
+        dis = new int[n+1];
         for (int i=0; i<m; i++) {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             graph.get(a).add(b); //3번 arrayList에 5를 add 시켜라 이런 의미
         }
-        ch[1] = 1;
-        T.DFS(1);
-        System.out.println(answer);
+        T.BFS(1); //너비 우선 탐색 1번 정점부터 출발해라
+        for(int i=2; i<=n; i++) {
+            System.out.println(i+ " : "+dis[i]);
+        }
     }
 }
